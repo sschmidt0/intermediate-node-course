3b11c74b5b0634ecee957f60b77ab5dcf8bc3b84
sarah <sschmidt0@uoc.edu>
2021-07-29 16:00:41 +0200

    added post and get for specific user methods

--- server.js
+++ server.js
@@ -1,24 +1,78 @@
 const express= require('express');
+const cors = require('cors');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
+const User = require('./models/User');
 
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// imports our database credentials (stored separately for security)
+const db = require('./config/keys').mongoURI;
+
+// initializes our database using the credentials
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
-})
+});
+
+app.get('/test', (req, res) => res.json({ msg: 'backend works' }));
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
-})
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
+});
+
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
